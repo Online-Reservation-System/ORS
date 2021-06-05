a4b443da1ae8fccda8b367a0800e1da534fc7bf6
nagababuthota984 <57616322+nagababuthota984@users.noreply.github.com>
2021-06-05 21:30:43 +0530

    Database has been created. An admin table(empty) is created.

--- orsystem/railways/models.py
+++ orsystem/railways/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Admin(models.Model):
+    name=models.CharField(max_length=50)
+    username=models.CharField(max_length=50)
+    password=models.CharField(max_length=50)
