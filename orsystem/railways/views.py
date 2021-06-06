a03379ed3adbc7a45ddd89086645f319bd8541c0
nagababuthota984 <57616322+nagababuthota984@users.noreply.github.com>
2021-06-06 10:05:24 +0530

    Using Custom Admin table is not feasible. Deleted that. will be using superuser credentials as Admin credentials.

--- orsystem/railways/views.py
+++ orsystem/railways/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import auth,User
 
 # Create your views here.
 def Welcome(request):
     return render(request,"Welcome.html")
 def AdminLogin(request):
-    return render(request,"AdminLogin.html")
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return render(request,"Welcome.html")
+        else:
+            return render(request,"AdminLogin.html")
+            
+    else:
+        return render(request,"AdminLogin.html")
