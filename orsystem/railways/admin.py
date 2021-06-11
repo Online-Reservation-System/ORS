2623c8eafa168c0c92153f691461cfc966beb9eb
nagababuthota984 <57616322+nagababuthota984@users.noreply.github.com>
2021-06-11 19:39:28 +0530

    User Login and User Registration has been added succesfully. Partially functional user trainlist page has been added.

--- orsystem/railways/admin.py
+++ orsystem/railways/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Admin,Train,Ticket
+from .models import Admin,Train,Ticket,AppUser
 
 # Register your models here.
 admin.site.register(Admin)
 admin.site.register(Train)
 admin.site.register(Ticket)
+admin.site.register(AppUser)
--- orsystem/railways/models.py
+++ orsystem/railways/models.py
@@ -16,9 +16,19 @@ class Train(models.Model):
     totalseats = models.IntegerField()
     filled = models.IntegerField()
     status = models.CharField(max_length=20)
+
+class AppUser(models.Model):
+    user_id  = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='UserID')
+    name = models.CharField(max_length=30)
+    username= models.CharField(max_length=20,unique=True)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50,unique=True)
+    phone=models.BigIntegerField()
 class Ticket(models.Model):
     ticket_id = models.CharField(max_length=10,primary_key=True)
     trainid = models.ForeignKey(Train, verbose_name="TrainID", on_delete=models.CASCADE)
+    passanger_id = models.ForeignKey(AppUser,verbose_name="UserID",on_delete=models.CASCADE)
     passanger_name = models.CharField(max_length=100)
     status = models.CharField(max_length=20)
     
+
