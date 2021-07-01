f7723a3e1fb556042e4ba7657b63a94bef61df31
senasowseelya <57617903+senasowseelya@users.noreply.github.com>
2021-07-01 13:57:04 +0530

    Update settings.py

--- orsystem/settings.py
+++ orsystem/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1(^i#x=krkc@ib398-kedamt5v^*@9-7!==3)%az)=h7dtbp3g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
