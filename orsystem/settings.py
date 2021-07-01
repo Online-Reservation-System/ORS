c2a95c9f2676b1b718eabcde1a06da97734c55d5
senasowseelya <57617903+senasowseelya@users.noreply.github.com>
2021-07-01 13:29:57 +0530

    Update settings.py

--- orsystem/settings.py
+++ orsystem/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1(^i#x=krkc@ib398-kedamt5v^*@9-7!==3)%az)=h7dtbp3g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
