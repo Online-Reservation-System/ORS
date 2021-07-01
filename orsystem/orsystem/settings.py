55fd57c6f861583e01f06d65fb21c204f54b1c3e
nagababuthota984 <57616322+nagababuthota984@users.noreply.github.com>
2021-07-01 12:28:03 +0530

    hosting purpose

--- orsystem/orsystem/settings.py
+++ orsystem/orsystem/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!6@6p(#j^q293sn7bl(0*m-(39o0x4h7a*z@tt2c^pqi4i^j*6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS += ('mathfilters', )
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
